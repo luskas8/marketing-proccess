32ba02e9d074320eb6fb86dec8d0c3dfb47ac03d
luskas8 <gnomo1049@gmail.com>
2023-05-11 13:18:05 -0300

    init pipedrive webhook app manager

--- app/urls.py
+++ app/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('contact/', views.contact_view, name='contact'),
     path('api/rdstation/', include('rdstation.urls'), name='rdstation'),
-    path('pipedrive/webhook/', views.pipedriveWebhook, name='pipedrive_webhook'),
+    path('api/pipedrive/', include('pipedrivecrm.urls'), name='pipedrive'),
 ]
--- marketingproccess/settings.py
+++ marketingproccess/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'app',
     'rdstation',
+    'pipedrivecrm',
     'rest_framework',
 ]
 
