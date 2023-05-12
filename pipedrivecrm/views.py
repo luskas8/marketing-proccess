9a3ed119a8e501abc843aaa76d3ed1c7411120cb
luskas8 <gnomo1049@gmail.com>
2023-05-12 12:58:12 -0300

    authorizarion to webhooks

--- pipedrivecrm/views.py
+++ pipedrivecrm/views.py
@@ -1,11 +1,20 @@
-import os
 import json
+import os
+
 import requests
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import (api_view, authentication_classes,
+                                       permission_classes)
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 
 from app.sendmail import sendmail
 
+
+@api_view(['GET', 'POST'])
+@authentication_classes([])
+@permission_classes([AllowAny])
 def webhook_deal(request):
     if request.method == 'GET':
         return Response({"message": "Pipedrive webhook deal working"}, status=status.HTTP_200_OK)
@@ -17,6 +26,9 @@ def webhook_deal(request):
     
     return Response({"message": "Pipedrive webhook deal working"}, status=status.HTTP_200_OK)
 
+@api_view(['GET', 'POST'])
+@authentication_classes([])
+@permission_classes([AllowAny])
 def webhook_person(request):
     if request.method == 'GET':
         return Response({"message": "Pipedrive webhook person working"}, status=status.HTTP_200_OK)
