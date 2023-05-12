572bf3d940cfe0febf8fee2308dcbd0db4ad0f11
luskas8 <gnomo1049@gmail.com>
2023-05-12 12:39:00 -0300

    adjust pipedrive webhook

--- pipedrivecrm/views.py
+++ pipedrivecrm/views.py
@@ -10,16 +10,19 @@ def webhook_deal(request):
     if request.method == 'GET':
         return Response({"message": "Pipedrive webhook deal working"}, status=status.HTTP_200_OK)
     
-    sendmail("luskanjos@gmail.com", "Pipedrive webhook deal working", "Pipedrive webhook deal working\nhttps://www.luskas8.xyx/forms/processo?email=luskanjos@gmail.com")
-    request_body = json.loads(request.body.decode('utf-8'))
-    print(request_body)
+    elif request.method == 'POST':
+        sendmail("luskanjos@gmail.com", "Pipedrive webhook deal working", "Pipedrive webhook deal working\nhttps://www.luskas8.xyx/forms/processo?email=luskanjos@gmail.com")
+        request_body = json.loads(request.body.decode('utf-8'))
+        print(request_body)
+    
     return Response({"message": "Pipedrive webhook deal working"}, status=status.HTTP_200_OK)
 
 def webhook_person(request):
     if request.method == 'GET':
         return Response({"message": "Pipedrive webhook person working"}, status=status.HTTP_200_OK)
     
-    request_body = json.loads(request.body.decode('utf-8'))
-    print(request_body)
+    elif request.method == 'POST':
+        request_body = json.loads(request.body.decode('utf-8'))
+        print(request_body)
     
     return Response({"message": "Pipedrive webhook person working"}, status=status.HTTP_200_OK)
