bcffbbf1fbb1271a62a344d6311424f3d3e634ea
luskas8 <gnomo1049@gmail.com>
2023-05-13 15:35:11 -0300

    update pipedrive urls

--- pipedrivecrm/urls.py
+++ pipedrivecrm/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('api/pipedrive/webhook_deal', views.webhook_deal, name='pipedrive_weebhook_deal'),
-    path('api/pipedrive/webhook_person', views.webhook_person, name='pipedrive_weebhook_person'),
+    path('webhook_deal', views.webhook_deal, name='pipedrive_weebhook_deal'),
+    path('webhook_person', views.webhook_person, name='pipedrive_weebhook_person'),
 ]
