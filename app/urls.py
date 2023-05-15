47aa397c362109ec2b878270894de29be85e22ca
luskas8 <gnomo1049@gmail.com>
2023-05-15 11:09:01 -0300

    fix url

--- app/urls.py
+++ app/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', RedirectView.as_view(url="contact", permanent=False), name='index'),
-    path('contact', views.contact_view, name='contact'),
+    path('contact/', views.contact_view, name='contact'),
     path('forms/proccess/<str:personId>', views.additional_info_view, name='additional_info'),
     path('api/rdstation/', include('rdstation.urls'), name='rdstation'),
     path('api/pipedrive/', include('pipedrivecrm.urls'), name='pipedrive'),
